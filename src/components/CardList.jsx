540563ef73dca401192f382d170d634a1c897135
sidulanaksekolahan <mirfan.duri@gmail.com>
2021-05-12 20:16:32 +0700

    first commit

---
+++ src/components/CardList.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Card from './Card';
+
+const CardList = ({ robots }) => {
+
+    return (
+        <div>
+            {robots.map(robot => {
+                return <Card id={robot.id} name={robot.name} email={robot.email} key={robot.id}/>
+            })}
+        </div>
+    );
+};
+
+export default CardList;
---
+++ src/containers/App.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import './App.css'
+import Scroll from '../components/Scroll';
+
+class App extends Component {
+
+    constructor() {
+        super()
+        this.state = {
+            robots: [],
+            searchField: ''
+        }
+    }
+
+    componentDidMount() {
+        // fetch data from end point and update the state of robots
+        fetch('http://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(json => this.setState({
+                robots: json
+            }))
+    }
+
+    onSearchChange = (event) => {
+        // update searchField based on onChange input
+        this.setState({
+            searchField: event.target.value
+        })
+    }
+    
+    render() {
+
+        const { robots, searchField } = this.state;
+        // filter robot based on searchField
+        const filteredRobots = robots.filter(robot => {
+            return robot.name.toLocaleLowerCase().includes(
+                searchField.toLocaleLowerCase());
+        })
+
+        return !robots.length ?
+                <h1>Loading</h1>
+            : 
+            (
+            <div className="tc">
+                <h1 className="f1">RoboFriends</h1>
+                <SearchBox searchChange={this.onSearchChange}/>
+                <Scroll>
+                    <CardList robots={filteredRobots}/>
+                </Scroll>
+            </div>
+        );
+    }
+}
+ 
+export default App;
